39fbc04f781df8647b953d51c0ea2ba094993817
Abuzar Baig Mirza <baigabuzar7@gmail.com>
2024-01-15 09:27:08 +0530

    Changes done

--- loadDataAndUI.js
+++ loadDataAndUI.js
@@ -15,30 +15,53 @@ function handleItemClick(event) {
     }
 }
 
-// Function to add items to the list
-function addItemToList(listId, link, text) {
+
+// // Function to add items to the list
+function addItemToList(listId, link, text, associatedObject={}) {
     const list = document.getElementById(listId);
     const listItem = document.createElement('li');
     const anchor = document.createElement('a');
+    
+    // Set href, text content, and target for the anchor
     anchor.href = link;
     anchor.textContent = text;
-    anchor.target = "_blank"
+    anchor.target = "_blank";
+    
+    // Set the associated object as a data attribute
+    anchor.dataset.associatedObject = JSON.stringify(associatedObject);
+
     listItem.appendChild(anchor);
     list.appendChild(listItem);
 }
 
-let isTimerUpdated = false;
+// Example usage:
+// const myObject1 = { id: 1, name: 'Object 1' };
+// const myObject2 = { id: 2, name: 'Object 2' };
 
-// Add items to the Emergency list
-addItemToList('emergencyList', 'https://www.google.com', 'Google');
-addItemToList('emergencyList', 'https://www.example.com', 'Example');
+// addItemToList('emergencyList', 'https://example.com', 'Item 1', myObject1);
+// addItemToList('sameDayList', 'https://example.com', 'Item 2', myObject2);
 
-// Add items to the Same Day list
-addItemToList('sameDayList', 'https://www.apple.com', 'Apple');
-addItemToList('sameDayList', 'https://www.microsoft.com', 'Microsoft');
+
+function createMapUrlAndAddItemToList(listId, responseData, cloudHost) {
+    if (responseData && responseData.data && responseData.data.length) {
+        let paramsArray = responseData.data.forEach((dataObj) => {
+            let {act, scall, add} = dataObj;
+            // URL to make - https://us.coresystems.net/shell/#/planning-dispatching/map/date/latitude,longitude/activities/activityId
+            let {latitude, longitude} = add.location ? add.location : {latitude: 0, longitude: 0}; // if there's no lat and long data, we're defauilting to 0 & 0 so as to avoid the failure case
+            let {id: activityId, createDateTime, code} = act;
+            let mapDate = createDateTime ? createDateTime.substring(0, 10) : new Date().toISOString().substring(0, 10);
+            let mapUrlForCurrentActivity = `https://${cloudHost}/shell/#/planning-dispatching/map/${mapDate}/${latitude},${longitude}/activities/${activityId}`;
+            let itemText = `${code} - Map`;
+            addItemToList(listId, mapUrlForCurrentActivity, itemText);
+        })
+    }
+}
+
 
 
 document.getElementById('timeLink').addEventListener('click', onTimeClick);
+let isTimerUpdated = false;
+let previousInputValue;
 
 function onTimeClick() {
     if (!isTimerUpdated) {
@@ -49,9 +72,11 @@ function onTimeClick() {
     
         // Create a new input control
         const inputControl = document.createElement('input');
+        inputControl.setAttribute('id','inputId');
         inputControl.placeholder = 'Enter time...';
         inputControl.type = 'text'; // Use 'text' to allow pattern validation
         inputControl.value = 10;
+        previousInputValue = 10;
     
         // Validate and sanitize input on each change
         inputControl.addEventListener('input', function () {
@@ -96,11 +121,14 @@ function onTimeClick() {
 function onTimerSave() {
     document.getElementById('timerId').style.display = 'none';
     enableControls();
+    // Here we've to fetch the data with the previousInputValue as the timer
 }
 
 function onTimerCancel() {
+    document.getElementById('inputId').value = previousInputValue;
     document.getElementById('timerId').style.display = 'none';
     enableControls();
+    // Dont fetch any data
 }
 
 
--- utils.js
+++ utils.js
@@ -2,12 +2,48 @@ function updateWarning(text) {
     document.getElementById("greet").innerHTML = text
 }
 
-// Loop before a token expire to fetch a new one
-function initializeRefreshTokenStrategy(shellSdk, auth) {
+function isInsideShell(FSMShell) {
+    const { ShellSdk, SHELL_EVENTS } = FSMShell;
+    if (!ShellSdk.isInsideShell()) {
+        updateWarning('⚠️ Unable to reach shell event API');
+    } else {
+        // Initialise ShellSDK to connect with parent shell library
+        const shellSdk = ShellSdk.init(parent, '*');
 
+        // Initialise the extension by requesting the fsm context
+        shellSdk.emit(SHELL_EVENTS.Version1.REQUIRE_CONTEXT, {
+            clientIdentifier: '000179c6-c140-44ec-b48e-b447949fd5c9',
+            clientSecret: '46342ddc-22aa-4f11-98a7-e9032b55477f',
+            auth: {
+                response_type: 'token'  // request a user token within the context
+            }
+        });
+
+        // Callback on FSM context response
+        shellSdk.on(SHELL_EVENTS.Version1.REQUIRE_CONTEXT, (event) => {
+            const {
+                // extract required context from event content
+                cloudHost,
+                account,
+                company,
+                user,
+                auth,
+                accountId,
+                companyId
+            } = JSON.parse(event);
+
+            // Access_token has a short life stpan and needs to be refreshed before expiring
+            // Each extension need to implement its own strategy to fresh it.
+            initializeRefreshTokenStrategy(shellSdk, auth, JSON.parse(event));
+        });
+    }
+}
+
+// Loop before a token expire to fetch a new one
+async function initializeRefreshTokenStrategy(shellSdk, auth, comapnyObject) {
     shellSdk.on(SHELL_EVENTS.Version1.REQUIRE_AUTHENTICATION, (event) => {
         sessionStorage.setItem('token', event.access_token);
-        setTimeout(() => fetchToken(), (event.expires_in * 1000) - 5000);
+        setTimeout(() => fetchToken(), (event.expires_in * 1000) - 10000);
     });
 
     function fetchToken() {
@@ -17,37 +53,43 @@ function initializeRefreshTokenStrategy(shellSdk, auth) {
     }
 
     sessionStorage.setItem('token', auth.access_token);
-    setTimeout(() => fetchToken(), (auth.expires_in * 1000) - 5000);
+    setTimeout(() => fetchToken(), (auth.expires_in * 1000) - 10000);
+
+    await fetchData('emergencyList', comapnyObject); // For Emergency orders
+    await fetchData('sameDayList', comapnyObject); // For Same day orders
 }
 
-function getActivityContext(cloudHost, account, company, activity_id, obj) {
-    let addressUrl = `https://ds.coresuite.com/ds/api/directory/v1/accounts/${encodeURIComponent(account)}`;
+async function fetchData(listId, comapnyObject) {
+    // Next call for loading the data asynchronously time to time
+    let loadDataTimePeriod = Number(document.getElementById("inputId").value) * 60 * 1000; // time in milli seconds i.e 1min * 60sec * 1000ms
+    setTimeout((comapnyObject) => {
+        fetchData(comapnyObject);
+    }, loadDataTimePeriod, comapnyObject);
+
+    const { cloudHost, account, company, accountId, companyId } = comapnyObject; // extract required context from event content
+
     const header = {
         "Content-Type": "application/json",
         "X-Client-ID": "000179c6-c140-44ec-b48e-b447949fd5c9",
         "X-Client-Version": "1.0",
         "Authorization": `bearer ${sessionStorage.getItem('token')}`,
-        "X-Account-ID": obj.accountId,
-        "X-Company-ID": obj.companyId
+        "X-Account-ID": accountId,
+        "X-Company-ID": companyId
     };
-    let url = `https://${cloudHost}/api/data/v4/Activity/${activity_id}?dtos=Activity.43&account=${account}&company=${company}`
-    let body = JSON.stringify({
-        "query" : "SELECT a from Activity a"
-    });
+    let url = `https://${cloudHost}/api/query/v1?account=${account}&company=${company}&dtos=Activity.43;ServiceCall.27;Address.22`
+    let body = JSON.stringify({ "query": "SELECT act, scall, add FROM Activity act JOIN ServiceCall scall ON scall.id = act.object.objectId JOIN Address add ON add.id = act.address"});
     let method = 'POST';
-    return new Promise(resolve => {
-        // Fetch Activity object
-        fetch(addressUrl, {
-            method: "GET",
-            headers: header
-        }).then(response => {
-            debugger
-            response.json()
-        }).then(json => {
-            console.log(json);
-        }).catch(err => {
-            console.log(err);
-        })
-    })
-}
 
+    try {
+        let response = await fetch(url, {
+            method: method,
+            headers: header,
+            body: body
+        });
+        let jsonResponse = await response.json();
+        createMapUrlAndAddItemToList(listId, jsonResponse, cloudHost);
+        return true
+    } catch (error) {
+        return alert(`Failed to fetch the data due to ${error} \n Reload the page manually`);
+    }
+}
